b8b5932c51f321cf8254b1e38cb409cf25639a5c
Scott Bouchard <scottbouchard09@gmail.com>
2019-10-29 01:21:14 -0400

    add command line parser to server and client

--- src/main/java/UDPClient/Client.java
+++ src/main/java/UDPClient/Client.java
@@ -1,8 +1,15 @@
 package UDPClient;
 
+import CLIParsing.Client.GetParser;
+import CLIParsing.Client.PostParser;
+import com.beust.jcommander.JCommander;
+
 public class Client {
 
     public static void main(String[] args) {
-
+        GetParser getParser = new GetParser();
+        PostParser postParser = new PostParser();
+        JCommander parser = JCommander.newBuilder().addCommand("get", getParser).addCommand("post", postParser).build();
+        parser.parse(args);
     }
 }
--- src/main/java/UDPServer/Server.java
+++ src/main/java/UDPServer/Server.java
@@ -1,8 +1,13 @@
 package UDPServer;
 
+import CLIParsing.Server.ServerParser;
+import com.beust.jcommander.JCommander;
+
 public class Server {
 
     public static void main(String[] args) {
-        
+        ServerParser serverParser = new ServerParser();
+        JCommander parser = JCommander.newBuilder().addObject(serverParser).build();
+        parser.parse(args);
     }
 }
